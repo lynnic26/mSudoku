432e53318ac4c4fe163b9425241dd029c845b18a
Lynnic Kwan <webfrontend@outlook.com>
2016-12-19 18:01:46 +0800

    first commit

---
+++ js/sudoku.js
@@ -0,0 +1,47 @@
+var Sudoku = (function($) {
+    var _instance
+      , _game
+      , defaultConfig = {};
+    function init(config) {
+        conf = $.extend({}, defaultConfig, config);
+        _game = new Game(conf);
+
+        // public method
+        return {
+            getGameBoard: function() {
+                return _game.bindUI();
+            }
+        }
+    }
+    function Game(config) {
+        // Game initialization
+    }
+    Game.prototype = {
+
+        // Game logic
+        bindUI: function() {
+            var $table = $('<table>').addClass('sudoku-container');
+            for(var i = 0; i < 9; i ++) {
+                var $tr = $('<tr>');
+                for(var j = 0; j < 9; j ++) {
+                    var $td = $('<td>');
+                    var $input = $('<input>');
+                    $td.append($input);
+                    $tr.append($td);
+                }
+                $table.append($tr);
+            }
+            return $table;
+        }
+    }
+    return {
+
+        // Get the Singleton instance
+        getInstance: function(config) {
+            if(!_instance) {
+                _instance = init(config);
+            }
+            return _instance;
+        }
+    }
+})(jQuery);
